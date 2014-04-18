bdf65ac582861c0363cc7e062232419f747bc696
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-04-18 11:03:10 +0800

    parse stats;

--- receive.py
+++ receive.py
@@ -1,4 +1,28 @@
+import re
 import socket
+
+def parse(content):
+  lines = content.splitlines()
+  stats = []
+  time = 0
+  for line in lines:
+    T = re.search('T:(\d+)', line)
+    if T: time = T.group(1)
+
+    I = re.search('I:([a-f0-9\-]+)', line)
+    if not I: continue
+    N = re.search('N:([^ ]+)', line)
+    D = re.search('D:([0-9]+)', line)
+    U = re.search('U:([0-9]+)', line)
+
+    stats.append((
+      int(time),
+      I.group(1),
+      N.group(1) if N else None,
+      int(D.group(1)) if D else 0,
+      int(U.group(1)) if U else 0))
+
+  return stats
 
 if __name__ == "__main__":
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
@@ -6,9 +30,6 @@ if __name__ == "__main__":
   sock.listen(5);
   while 1:
     connection, address = sock.accept()
-    data = ''
-    while 1:
-      buf = connection.recv(64)
-      if not buf: break
-      data += buf
-    print data
+    data = connection.recv(1024)
+    stats = parse(data)
+    print stats[0][0]
