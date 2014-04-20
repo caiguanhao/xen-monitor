9ec27c986bfa99c1dab4f5bad2939882c4e4ebd5
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-04-20 17:50:07 +0800

    stats for all make a total stat bar;

--- web/js/application.js
+++ web/js/application.js
@@ -22,7 +22,11 @@ factory('Socket', ['$window', function($window) {
 }]).
 
 service('Servers', [function() {
-  this.allServers = {};
+  this.colorBySpeed = function(speed) {
+    if (speed > 2000000) return 'success';
+    if (speed > 1000000) return 'warning';
+    return 'danger';
+  };
   this.colorByPercent = function(percent) {
     if (percent > 66) return 'success';
     if (percent > 33) return 'warning';
@@ -37,28 +41,45 @@ service('Servers', [function() {
     }
     return names[0];
   };
+  this.allServers = {};
+  this.allServersByColor = {};
+  this.lastTimeCountServersByColor = 0;
+  this.countServersByColor = function($scope) {
+    var cS = { success: 0, warning: 0, danger: 0 };
+    for (var group_name in this.allServersByColor) {
+      cS.success += this.allServersByColor[group_name].success || 0;
+      cS.warning += this.allServersByColor[group_name].warning || 0;
+      cS.danger += this.allServersByColor[group_name].danger || 0;
+    }
+    cS.total = cS.success + cS.warning + cS.danger;
+    cS.successPercent = Math.floor(cS.success / cS.total * 100);
+    cS.warningPercent = Math.floor(cS.warning / cS.total * 100);
+    cS.dangerPercent = 100 - cS.successPercent - cS.warningPercent;
+    $scope.colorStats = cS;
+  };
   this.updateServers = function($scope, group_name) {
     var groupServers = this.allServers[group_name] || {};
     var servers = [];
-    var top = {
-      download: 0,
-      upload: 0
-    };
+    var top = { download: 0, upload: 0 };
     for (var ipaddr in groupServers) {
       var server = groupServers[ipaddr];
       if (server.upload > top.upload) top.upload = server.upload;
       if (server.download > top.download) top.download = server.download;
     }
+    var byColor = {};
     for (var ipaddr in groupServers) {
       var server = groupServers[ipaddr];
       var uploadPercent = Math.floor(server.upload / top.upload * 100);
       var downloadPercent = Math.floor(server.download / top.download * 100);
+      var uploadColor = this.colorBySpeed(server.upload);
+      byColor[uploadColor] = byColor[uploadColor] || 0;
+      byColor[uploadColor]++;
       servers.push({
         IP: ipaddr,
         upload: server.upload,
         uploadText: Math.floor(server.upload / 1024) + ' KB/s',
-        uploadPercent:uploadPercent ,
-        uploadColor: this.colorByPercent(uploadPercent),
+        uploadPercent: uploadPercent,
+        uploadColor: uploadColor,
         download: server.download,
         downloadText: Math.floor(server.download / 1024) + ' KB/s',
         downloadPercent: downloadPercent,
@@ -71,6 +92,11 @@ service('Servers', [function() {
     });
     $scope.allServers = $scope.allServers || {};
     $scope.allServers[group_name] = servers;
+    this.allServersByColor[group_name] = byColor;
+    if ((+new Date) - this.lastTimeCountServersByColor > 3000) {
+      this.countServersByColor($scope);
+      this.lastTimeCountServersByColor = +new Date
+    }
     $scope.$apply();
   };
 }]).
