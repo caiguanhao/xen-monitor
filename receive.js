06e10e49246d311c13b74bccb2533f5f1f23a34a
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-04-30 17:18:41 +0800

    don't use 0 for errors;

--- receive.js
+++ receive.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
+var args = process.argv.slice(2);
+
+var VERBOSE = false;
+
+for (var i = 0; i < args.length; i++) {
+  switch (args[i]) {
+  case '-v':
+  case '--verbose':
+    VERBOSE = true;
+  }
+}
+
 var net = require('net');
 var redis = require('redis');
 var client = redis.createClient();
@@ -15,15 +27,24 @@ var server = net.createServer(function(sock) {
       var i;
       for (i = 0; i < stats.A.length; i++) {
         var A = stats.A[i];
-        if (A.U.length > MAX_NUMBER_LENGTH || A.D.length > MAX_NUMBER_LENGTH) {
-          continue;
+        if (A.U.length > MAX_NUMBER_LENGTH) {
+          if (VERBOSE) throw 'unexpected large number: ' + A.U;
+        }
+        if (A.D.length > MAX_NUMBER_LENGTH) {
+          if (VERBOSE) throw 'unexpected large number: ' + A.D;
         }
         S[A.I] = { U: +A.U, D: +A.D };
       }
       var DATA = {
         K: stats.V.map(function(x) { return x.IP || '127.0.0.1'; }),
-        U: stats.V.map(function(x) { return S[x.I] ? S[x.I].U : 0; }),
-        D: stats.V.map(function(x) { return S[x.I] ? S[x.I].D : 0; }),
+        U: stats.V.map(function(x) {
+          if (!S[x.I] && VERBOSE) throw 'unexpected S: ' + JSON.stringify(S);
+          return S[x.I].U
+        }),
+        D: stats.V.map(function(x) {
+          if (!S[x.I] && VERBOSE) throw 'unexpected S: ' + JSON.stringify(S);
+          return S[x.I].D
+        }),
         S: stats.V.map(function(x) { return x.PS || 'U'; }),
         I: stats.V.map(function(x) { return x.I || '0'; })
       };
