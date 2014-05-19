210335d63b5409b87b1a9c52e9cdf085282705ce
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-05-19 21:24:26 +0800

    update llks screenshot and server script;

--- llks/screenshot.sh
+++ llks/screenshot.sh
@@ -4,33 +4,50 @@ pgrep server.sh >/dev/null || ( ./server.sh & )
 
 IMAGES="/srv/xen-monitor/images"
 mkdir -p $IMAGES
-rm -f $IMAGES/*.png
 
-(IFS=$'\n'; for VM in `xl list`; do
-  IFS=$' \t'; VM=($VM)
-  if [[ ${VM[1]} -gt 0 ]]; then
-    CMD="vncdo -s localhost:"
-    CMD="${CMD}$((`xenstore-read /local/domain/${VM[1]}/console/vnc-port` - 5900))"
-    CAPTURE="${CMD} capture ${IMAGES}/${VM[0]}-full.png"
-    $CAPTURE
-  fi
-done)
+(
+  JSON="[{\"name\":\"info\",\"time\":\"`date +%s`\"},"
+  JSON="$JSON{\"name\":\"montage\",\"full\":\"/images/montage.png\"}"
+  IFS=$'\n'
+  for VM in `xl list`; do
+    IFS=$' \t'
+    VM=($VM)
+    if [[ ${VM[1]} -gt 0 ]]; then
+      CMD="vncdo -s localhost:"
+      CMD="${CMD}$((`xenstore-read /local/domain/${VM[1]}/console/vnc-port` - 5900))"
+      CAPTURE="${CMD} capture ${IMAGES}/${VM[0]}-full.png"
+      $CAPTURE
+      JSON="$JSON,{\"name\":\"${VM[0]}\","
+      JSON="$JSON\"full\":\"/images/${VM[0]}-full.png\","
+      JSON="$JSON\"mini\":\"/images/${VM[0]}-mini.png\"}"
+    fi
+  done
+  JSON="$JSON]"
+  echo $JSON > ${IMAGES}/images.json
+)
 
 cat <<PY | python2.7 -
-import glob, math, Image
+import os, glob, time, math, Image, ImageFont, ImageDraw
+now = time.strftime("%H:%M:%S", time.gmtime(time.time() + 8 * 3600))
 files = glob.glob('$IMAGES/*-full.png')
 files.sort(key=lambda file: int(file.replace('-','.').split('.')[-3]))
-cols = 2
+cols = 4
 rows = int(math.ceil(float(len(files)) / cols))
+off_y = 16
 width = 200
 height = 150
-all = Image.new('RGB', (width * cols, height * rows))
+font = ImageFont.truetype('Ubuntu-L.ttf', 12)
+all = Image.new('RGB', (width * cols, height * rows + off_y))
 for i, file in enumerate(files):
+  filename = os.path.basename(file)
   min = Image.open(file)
   min = min.crop((600, 450, 800, 600))  # bottom right corner
-  min.save(file[:file.rfind('-full.png')] + '-min.png')
+  min.save(file[:file.rfind('-full.png')] + '-mini.png')
   top = int(math.floor(float(i) / cols)) * height
   left = i % cols * width
-  all.paste(min, (left, top, left + width, top + height))
+  all.paste(min, (left, top + off_y, left + width, top + height + off_y))
+  draw = ImageDraw.Draw(all)
+  draw.text((left + 10, 0), filename[:filename.find('-')], (255,255,255), font=font)
+  draw.text((left + 140, 0), now, (255,255,255), font=font)
 all.save('$IMAGES/montage.png')
 PY
--- llks/server.sh
+++ llks/server.sh
@@ -3,10 +3,22 @@
 ROOT="/srv/xen-monitor"
 
 cd $ROOT
+rm -f $ROOT/images/*.json $ROOT/images/*.png
 
-CMD="python2.7 -m SimpleHTTPServer 54321"
-
-until $CMD >/dev/null 2>/dev/null; do
+until (
+cat <<PY | python2.7 -
+import SimpleHTTPServer
+import SocketServer
+class CustomServer(SocketServer.TCPServer):
+  allow_reuse_address = True
+class CustomHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+  def end_headers(self):
+    self.send_header("Access-Control-Allow-Origin", "*")
+    SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+httpd = CustomServer(('', 54321), CustomHTTPRequestHandler)
+httpd.serve_forever()
+PY
+) >/dev/null 2>/dev/null; do
   sleep 3
 done
 
