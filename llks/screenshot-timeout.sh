c3e4e2968be76730ac612a5c6285adb4decef79e
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-06-02 00:40:23 +0800

    prevent too-fast screenshot generation;

--- llks/screenshot-timeout.sh
+++ llks/screenshot-timeout.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # Take screenshot persistently
-# Use Ctrl-C or `kill -f screenshot-timeout.sh` to terminate
+# Use Ctrl-C or `pkill -f screenshot-timeout.sh` to terminate
 
-TIMEOUT=30
+MAXTIME=30
+MINTIME=5
+
 DIR=$(dirname -- "$0")
 SCRIPT="$DIR/screenshot.sh"
 
 alarm() {
-  perl -e 'alarm shift; exec @ARGV' "$@";
+  perl -e 'alarm shift; exec @ARGV' "$@"
 }
 
 while :; do
-  alarm $TIMEOUT $SCRIPT
-  sleep 1
+  STARTTIME=$(date +%s)
+  alarm $MAXTIME $SCRIPT >/dev/null 2>/dev/null
+  ENDTIME=$(date +%s)
+  TOSLEEP=$(($MINTIME - $ENDTIME + $STARTTIME))
+  if [[ $TOSLEEP -lt 1 ]]; then
+    TOSLEEP=1
+  fi
+  sleep $TOSLEEP
 done
