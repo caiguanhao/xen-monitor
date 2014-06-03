406720d6554a7315e9a5ae739c7114303b65eb13
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-06-03 15:21:12 +0800

    add attach and detach command;

--- llks/command.sh
+++ llks/command.sh
@@ -65,6 +65,11 @@ convert_to_vncdo_command() {
   eval $VAR=\$OUT
 }
 
+leave() {
+  echo $@
+  exit 1
+}
+
 case $2 in
 get)
   convert_to_vncdo_command KEYCMD "${*:3}"
@@ -78,6 +83,53 @@ login)
   COMMAND="$VNC key space pause 1 key ctrl-alt-del pause 1 key alt-u"
   COMMAND="$COMMAND type $WINDOWSUSERNAME key alt-p $PASSWD key enter"
   ;;
+
+detach)
+  [[ $VMNAME == "" ]] && exit 1
+  DISKNAME="DTP_Windows_2003_c"
+  IFS=$','
+  VDI=($(xe vdi-list name-label="${DISKNAME}_Pending" --minimal))
+  [[ ${#VDI[@]} -ne 0 ]] && leave "Has pending disk."
+  VBD=($(xe vbd-list vm-name-label="${VMNAME}" vdi-name-label="${DISKNAME}" params=userdevice --minimal) 0)
+  IFS=$'\n'
+  MAXDEVICE=$(echo "${VBD[*]}" | sort -nr | head -n1)
+  IFS=$','
+  VBD=($(xe vbd-list vm-name-label="${VMNAME}" vdi-name-label="${DISKNAME}" userdevice="${MAXDEVICE}" params=uuid --minimal))
+  [[ ${#VBD[@]} -ne 1 ]] && leave "Can't find VBD."
+  VDI=($(xe vbd-list vm-name-label="${VMNAME}" vdi-name-label="${DISKNAME}" userdevice="${MAXDEVICE}" params=vdi-uuid --minimal))
+  [[ ${#VDI[@]} -ne 1 ]] && leave "Can't find VDI."
+  VM=($(xe vbd-list vm-name-label="${VMNAME}" vdi-name-label="${DISKNAME}" userdevice="${MAXDEVICE}" params=vm-uuid --minimal))
+  [[ ${#VM[@]} -ne 1 ]] && leave "Can't find VM."
+  xe vbd-unplug uuid="${VBD[0]}" && xe vbd-destroy uuid="${VBD[0]}" && \
+  xe vdi-param-set name-label="${DISKNAME}_Pending" uuid="${VDI[0]}"
+  CODE=$?
+  if [[ $CODE -eq 0 ]]; then
+    echo "Done."
+    exit 0
+  else
+    leave "Error with status code: $CODE."
+  fi
+  ;;
+
+attach)
+  [[ $VMNAME == "" ]] && exit 1
+  DISKNAME="DTP_Windows_2003_c"
+  IFS=$','
+  UUID=$(xe vm-list name-label="${VMNAME}" --minimal)
+  [[ $UUID == "" ]] && leave "Can't find VM."
+  VDI=($(xe vdi-list name-label="${DISKNAME}_Pending" --minimal))
+  [[ ${#VDI[@]} -ne 1 ]] && leave "Can't find VDI."
+  NEWVBD=$(xe vbd-create vm-uuid="${UUID}" device=autodetect vdi-uuid="${VDI[0]}" bootable=false mode=RW type=Disk)
+  [[ $NEWVBD == "" ]] && leave "Can't create new VBD."
+  xe vbd-plug uuid="${NEWVBD}" && xe vdi-param-set name-label="${DISKNAME}" uuid="${VDI[0]}"
+  if [[ $CODE -eq 0 ]]; then
+    echo "Done."
+    exit 0
+  else
+    leave "Error with status code: $CODE."
+  fi
+  ;;
+
 *)
   echo "Unknown command."
   exit 1
