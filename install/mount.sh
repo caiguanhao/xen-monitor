4e26111ef361e74dc2e7d60a8cff712b8ecf3b62
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-06-05 23:48:13 +0800

    update mount.sh;

--- install/mount.sh
+++ install/mount.sh
@@ -2,6 +2,8 @@
 
 SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=error -o PubkeyAuthentication=no"
 
+NUM=$2
+
 IFS=$'\n'
 for origline in $DIST; do
   IFS=$' \t'
@@ -9,16 +11,13 @@ for origline in $DIST; do
   if [[ ${#line[@]} -eq 2 ]]; then
     echo Processing ${line[0]}...
     CMD="
-if mount | grep /Media >/dev/null; then
-  echo -e \"\033[32mAlready mounted.\033[0m\"
-else
-  mkdir -p /Media
-  echo Mounting...
-  mount -t cifs \
-    -o username=${WINDOWSUSERNAME},password=${WINDOWSPASSWORD} \
-    //${line[0]%.*}.$((${line[0]##*.} + 1))/Media /Media
-  echo Mounted.
-fi
+IP=${line[0]%.*}.$((${line[0]##*.} + $NUM))
+mount | grep //\$IP | awk '{ print \$3 }' | xargs --no-run-if-empty umount
+mkdir -p /Windows/${NUM}
+mount -t cifs \
+  -o username=${WINDOWSUSERNAME},password=${WINDOWSPASSWORD} \
+  //\$IP/Media /Windows/${NUM}
+mount | grep //\$IP
 "
     sshpass -p "${line[1]}" $SSH root@${line[0]} "$CMD"
     CODE=$?
